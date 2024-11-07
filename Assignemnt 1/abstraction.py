c1faee457c4a9a9a4e0babeb4d8c51abdb63c53d
Hagen Leitner <hagen.leitner@student.tugraz.at>
2024-11-07 12:40:05 +0100

    Update bilateral, very slow

--- Assignemnt 1/abstraction.py
+++ Assignemnt 1/abstraction.py
@@ -41,7 +41,6 @@ def luminance_quantization(im):
 
     return quantized_luminance_image
 
-
 def bilateral_gaussian(im):
     # Radius of the Gaussian filter
     r = int(2 * sigma_s) + 1
@@ -50,26 +49,46 @@ def bilateral_gaussian(im):
     Implement the bilateral Gaussian filter (Eq. 3).
     Apply it to the padded image.
     '''
-
-    hight, width, channel = im.shape
-
+    print(type(padded[0][0]))
+    # np.convolve(mode='same')
+    
     win_size = 2 * r + 1
-    windows_neighborhoods_pixels = ns.sliding_window_view(padded, (win_size, win_size, channel))
+    win_shape = (win_size, win_size, 3)
+    windows_neighborhoods_pixels = ns.sliding_window_view(padded, window_shape=win_shape)
 
     y, x = np.mgrid[-r:r, -r:r]
     gaussian_spatial_weights = np.exp(-(x ** 2 + y ** 2) / (2 * sigma_s ** 2))
-
-
-
-
-
-    return im
+    M, N, LABdim = im.shape
+    # center of window = current pixel 
+    p = np.array([r + 1, r + 1])
+    U = np.ones_like(im) 
+    # cur_window = windows_neighborhoods_pixels[0][0][0]
+    for x_coord in range(0, 400):
+        print(x_coord)
+        for y_coord in range(0, 254): 
+            print(y_coord)
+            cur_window = windows_neighborhoods_pixels[x_coord][y_coord][0]
+            sum_top = [0,0,0]
+            sum_bottom = 0
+            for i in range(0, 17):
+                for j in range(0, 17):
+                    if  (i == r + 1) and (j == r + 1):
+                        continue    
+                    q = np.array([i, j])
+                    F_p = np.array(cur_window[p[0]][p[1]])
+                    F_q = np.array(cur_window[q[0]][q[1]])
+                    pixel_weights = np.exp(-((np.linalg.norm(p - q) / (2 * sigma_s ** 2)) - (np.linalg.norm(F_p - F_q) / (2 * sigma_r ** 2))))
+                    sum_top += pixel_weights * F_q
+                    sum_bottom += pixel_weights
+            U[x_coord][y_coord] = sum_top / sum_bottom
+    return U
 
 
 def abstraction(im):
     filtered = skc.rgb2lab(im)
     for _ in range(n_e):
         filtered = bilateral_gaussian(filtered)
+    imageio.imsave('filtered.png', skc.lab2rgb(filtered[0]))
     edges = edge_detection(filtered[:, :, 0])
 
     for _ in range(n_b - n_e):
@@ -83,7 +102,7 @@ def abstraction(im):
 
 if __name__ == '__main__':
     # Algorithm
-    n_e = 2
+    n_e = 1
     n_b = 4
     # Bilateral Filter
     sigma_r = 4.25  # "Range" sigma
@@ -96,6 +115,7 @@ if __name__ == '__main__':
     n_bins = 10
     phi_q = 0.7
 
-    im = imageio.imread('./girl.png') / 255.
+    im = imageio.imread('./Assignemnt 1/girl.png') / 255.
+    print(im[0][0])
     abstracted = abstraction(im)
-    imageio.imsave('abstracted.png', np.clip(abstracted, 0, 1))
+    # imageio.imsave('abstracted.png', np.clip(abstracted, 0, 1))
